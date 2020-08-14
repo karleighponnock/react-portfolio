163ef8fa147d0cbb9242a079c7c1045d290646d2
karleighponnock <karleighponnock@gmail.com>
2020-08-13 23:26:32 -0400

    Updates

---
+++ precache-manifest.3714f402280013f67fbcb2949b48171d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eb0f9ee1cf6c0256b9e0acb023f0372",
+    "url": "/karleighponnock/react-portfolio/index.html"
+  },
+  {
+    "revision": "5cdef9e99952da6f6693",
+    "url": "/karleighponnock/react-portfolio/static/css/main.fce8cbbb.chunk.css"
+  },
+  {
+    "revision": "2565c8ba1ddb6d7c657e",
+    "url": "/karleighponnock/react-portfolio/static/js/2.b7cad495.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/karleighponnock/react-portfolio/static/js/2.b7cad495.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cdef9e99952da6f6693",
+    "url": "/karleighponnock/react-portfolio/static/js/main.daff0ee8.chunk.js"
+  },
+  {
+    "revision": "5d122a231dd9d222af42",
+    "url": "/karleighponnock/react-portfolio/static/js/runtime-main.064425b7.js"
+  },
+  {
+    "revision": "f5b0546d87205c958273a974ac536077",
+    "url": "/karleighponnock/react-portfolio/static/media/maze2.f5b0546d.jpg"
+  },
+  {
+    "revision": "9963a81107a2e0a89d7806ddb20403de",
+    "url": "/karleighponnock/react-portfolio/static/media/portrait.9963a811.jpg"
+  }
+]);
