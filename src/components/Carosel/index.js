0790aef23337ad94ae13cc4b4b2966fbbcfa5f97
karleighponnock <karleighponnock@gmail.com>
2020-08-17 16:53:12 -0400

    bmt added to portfolio

--- src/components/Carosel/index.js
+++ src/components/Carosel/index.js
@@ -7,42 +7,42 @@ const Projects = ({ project, setProject, name }) => {
         return (
 
             <Card>
-                <Card>
-                    <iframe className="frame" src="https://lamcnguyen89.github.io/Project-01--The-Electric-Art-Gallery/" width="100%" height="600px" frameborder="0" scrolling="yes"></iframe>
-                </Card>
                 <button className="browse" onClick={() => setProject(2)}> next </button>
                 <button className="browse" onClick={() => setProject(4)}> back </button>
+                <Card>
+                    <iframe className="frame" src="https://karleighponnock.github.io/BLACK-MEDICINE-TATU/" width="100%" height="600px" frameborder="0" scrolling="yes"></iframe>
+                </Card>
             </Card>
         )
     } else if (project === 2) {
         return (
             <Card>
-                <Card>
-                    <iframe className="frame" src="https://house-our-heroes.herokuapp.com/" width="100%" height="600px" frameborder="0" scrolling="yes"></iframe>
-                </Card>
                 <button className="browse" onClick={() => setProject(3)}> next </button>
                 <button className="browse" onClick={() => setProject(1)}> back </button>
+                <Card>
+                    <iframe className="frame" src="https://lamcnguyen89.github.io/Project-01--The-Electric-Art-Gallery/" width="100%" height="600px" frameborder="0" scrolling="yes"></iframe>
+                </Card>
             </Card>
         )
     } else if (project === 3) {
         return (
             <Card>
+                 <button className="browse" onClick={() => setProject(4)}> next </button>
+                <button className="browse" onClick={() => setProject(2)}> back </button>
                 <Card >
                     <iframe className="frame" src="https://protected-tor-91451.herokuapp.com/" width="100%" height="600px" frameborder="0" scrolling="yes" className="budget"></iframe>
                 </Card>
-                <button className="browse" onClick={() => setProject(4)}> next </button>
-                <button className="browse" onClick={() => setProject(2)}> back </button>
             </Card>
 
         )
     } else if (project === 4) {
         return (
             <Card>
+                <button className="browse" onClick={() => setProject(1)}> next </button>
+                <button className="browse" onClick={() => setProject(3)}> back </button>
                 <Card>
                     <iframe className="frame" src="https://shielded-hamlet-39897.herokuapp.com/" width="100%" height="600px" frameborder="0" scrolling="yes"></iframe>
                 </Card>
-                <button className="browse" onClick={() => setProject(1)}> next </button>
-                <button className="browse" onClick={() => setProject(3)}> back </button>
             </Card>
 
         )
